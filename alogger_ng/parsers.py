8a7cfc003a0f530902f78979bacfd8cd27a70b80
Sam Morrison <sam@vpac.org>
2009-01-22 00:03:18 +0000

    added more captured values

--- alogger_ng/parsers.py
+++ alogger_ng/parsers.py
@@ -18,10 +18,15 @@ start         | datetime |
 jobid	      | string   |
 cores	      | int      | number of cores
 jobname       | string   | Job name
+exit_status   | int      | Exit status
 
 Optional
 mem           | int      | memory used
 vmem          | int      | virtual memory used
+list_mem      | int      | memory requested
+list_vmem     | int      | virtual memory requested
+list_pmem     | int      | memory requested (per processor)
+list_pvmem    | int      | virtual memory requested (per processor)
 
 Raises value error if funky wall time
 
@@ -90,6 +95,51 @@ def pbs_to_dict(line):
         formatted_data['mem'] = 0
     formatted_data['vmem'] = int(data['resources_used.vmem'][:len(data['resources_used.vmem'])-2])
     
+    #pmem format 30gb or 400mb also have b and kb
+    try:
+        pmem_value = int(data['Resource_List.pmem'][:-2])
+        pmem_unit = data['Resource_List.pmem'][len(data['Resource_List.pmem'])-2:]
+        if units == 'gb':
+            formatted_data['list_pmem'] = pmem_value * 1024
+        elif units = 'mb':
+            formatted_data['list_pmem'] = pmem_value
+    except:
+        formatted_data['list_pmem'] = 0
+
+    try:
+        mem_value = int(data['Resource_List.mem'][:-2])
+        mem_unit = data['Resource_List.mem'][len(data['Resource_List.mem'])-2:]
+        if units == 'gb':
+            formatted_data['list_mem'] = mem_value * 1024
+        elif units = 'mb':
+            formatted_data['list_mem'] = mem_value
+    except:
+        formatted_data['list_mem'] = 0 
+
+    try:
+        vmem_value = int(data['Resource_List.vmem'][:-2])
+        vmem_unit = data['Resource_List.vmem'][len(data['Resource_List.vmem'])-2:]
+        if units == 'gb':
+            formatted_data['list_vmem'] = vmem_value * 1024
+        elif units = 'mb':
+            formatted_data['list_vmem'] = vmem_value
+    except:
+        formatted_data['list_vmem'] = 0 
+
+    try:
+        pvmem_value = int(data['Resource_List.pvmem'][:-2])
+        pvmem_unit = data['Resource_List.pvmem'][len(data['Resource_List.pvmem'])-2:]
+        if units == 'gb':
+            formatted_data['list_pvmem'] = pvmem_value * 1024
+        elif units = 'mb':
+            formatted_data['list_pvmem'] = pvmem_value
+    except:
+        formatted_data['list_pvmem'] = 0 
+
+
+    formatted_data['exit_status'] = data['Exit_status']
+
+
     formatted_data['ctime'] = datetime.fromtimestamp(int(data['ctime'])).isoformat(' ')
     formatted_data['qtime'] = datetime.fromtimestamp(int(data['qtime'])).isoformat(' ')
     formatted_data['etime'] = datetime.fromtimestamp(int(data['etime'])).isoformat(' ')
